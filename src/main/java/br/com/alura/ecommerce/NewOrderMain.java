6d330249a97887c7437d73e35463512554689d0a
Gustavo Bezerra <gustavobz305@gmail.com>
2020-02-02 18:15:33 -0300

    Criando arquitetura flexivel de Deserializers

--- src/main/java/br/com/alura/ecommerce/NewOrderMain.java
+++ src/main/java/br/com/alura/ecommerce/NewOrderMain.java
@@ -1,21 +1,26 @@
 package br.com.alura.ecommerce;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
 public class NewOrderMain {
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        try(var dispatcher = new KafkaDispatcher()) {
+        try(var orderDispatcher = new KafkaDispatcher<Order>()) {
+            try(var emailDispatcher = new KafkaDispatcher<Email>()) {
+                for (var i = 0; i < 10; i++) {
 
-            for (var i = 0; i < 10; i++) {
+                    var userId = UUID.randomUUID().toString();
+                    var orderId = UUID.randomUUID().toString();
+                    var amount = new BigDecimal(Math.random() * 5000 + 1);
+                    Order order = new Order(userId, orderId, amount);
+                    orderDispatcher.send("ECOMMERCE_NEW_ORDER", userId, order);
 
-                var key = UUID.randomUUID().toString();
-                var value = key + "123123,654,99";
-                dispatcher.send("ECOMMERCE_NEW_ORDER", key, value);
+                    var email = new Email("New Order", "Thank you for your order! We are processing your order!");
 
-                var email = "Thank you for your order! We are processing your order!";
-                dispatcher.send("ECOMMERCE_SEND_EMAIL", key, email);
+                    emailDispatcher.send("ECOMMERCE_SEND_EMAIL", userId, email);
+                }
             }
         }
     }
